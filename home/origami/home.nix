692a8de54fc12fcfa50eb90fd908756dea37561e
Cat <bodyglue4@gmail.com>
2025-11-02 23:13:29 -0500

    .

--- home/origami/home.nix
+++ home/origami/home.nix
@@ -46,5 +46,56 @@
   # settings
   dconf.enable = true;
 
+  # systemd services
+  systemd.user.services = let
+    start-with-graphical-session = Description: {
+      Unit = {
+        inherit Description;
+        After = ["graphical-session.target"];
+        PartOf = ["graphical-session.target"];
+        BindsTo = ["graphical-session.target"];
+        Requisite = ["graphical-session.target"];
+      };
+      Install.WantedBy = ["graphical-session.target" "niri-session.service"];
+    };
+  in {
+    wallpaper = start-with-graphical-session "Wallpaper service"
+    // {
+      Service = {
+        Type = "simple";
+        ExecStart = "swaybg -i ~/nixos-dotfiles/assets/wallpapers/mountain.jpg -m fill";
+        Restart = "on-failure";
+      };
+    };
+
+    idle = start-with-graphical-session "Idle service"
+    // {
+      Service = {
+        Type = "simple";
+        ExecStart = "swayidle -w timeout 601 'niri msg action power-off-monitors' timeout 600 'swaylock -f' before-sleep 'swaylock -f'";
+        Restart = "on-failure";
+      };
+    };
+
+    waybar = start-with-graphical-session "Waybar service"
+    // {
+      Service = {
+        Type = "simple";
+        ExecStart = "waybar";
+        Restart = "on-failure";
+      };
+    };
+
+
+    mako = start-with-graphical-session "Mako service"
+      // {
+        Service = {
+          Type = "simple";
+          ExecStart = "mako";
+          Restart = "on-failure";
+        };
+      };
+  };
+
   home.stateVersion = "25.05";
 }
