a8fbaecd96587c0847996ed722d508c82a3f2cf7
Cat <bodyglue4@gmail.com>
2025-11-02 21:58:52 -0500

    .

--- home/origami/config/niri/autostart.sh
+++ home/origami/config/niri/autostart.sh
@@ -1,22 +1,9 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+sleep 1
 
-set -e
-
-# Start Polkit GNOME authentication agent
-(sleep 2 && /nix/store/w8h0097yqh767gigdig05ydcbf51qc0y-polkit-gnome-0.105/libexec/polkit-gnome-authentication-agent-1) &
-
-# Set Wallpaper (Swaybg)
+(sleep 2 && /nix/store/.../polkit-gnome-authentication-agent-1) &
 swaybg -i ~/nixos-dotfiles/assets/wallpapers/mountain.jpg -m fill &
-
-# Set Waybar
 waybar &
-
-# Set Mako
 mako &
-
-# Spawn base Terminal
 alacritty &
-
-# Set Screenlocking & idle
-sway-audio-idle-inhibit &
-swayidle -w timeout 601 'niri msg action power-off-monitors' timeout 600 'swaylock -f' before-sleep 'swaylock -f'
+swayidle -w timeout 601 'niri msg action power-off-monitors' timeout 600 'swaylock -f' before-sleep 'swaylock -f' &
