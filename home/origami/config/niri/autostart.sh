344ba4cb6eb7a02f0802dd66b0d28fe27f1e3e5b
Cat <bodyglue4@gmail.com>
2025-11-02 21:00:49 -0500

    Wallpaper Init

--- home/origami/config/niri/autostart.sh
+++ home/origami/config/niri/autostart.sh
@@ -6,4 +6,4 @@ set -e
 (sleep 2 && /nix/store/w8h0097yqh767gigdig05ydcbf51qc0y-polkit-gnome-0.105/libexec/polkit-gnome-authentication-agent-1) &
 
 # Set wallpaper
-swww-daemon & sleep 1 && swww img ~/Pictures/Walls/mountain.jpg
+swww-daemon & sleep 1 && swww img ~/nixos-dotfiles/assets/wallpapers/mountain.jpg
