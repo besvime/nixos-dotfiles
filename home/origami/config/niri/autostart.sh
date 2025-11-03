2301405fde9f871048c9d87a5445c559d57ef87d
Cat <bodyglue4@gmail.com>
2025-11-02 22:58:20 -0500

    .

--- home/origami/config/niri/autostart.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-sleep 1
-
-swaybg -i ~/nixos-dotfiles/assets/wallpapers/mountain.jpg -m fill &
-waybar &
-mako &
-alacritty &
-swayidle -w timeout 601 'niri msg action power-off-monitors' timeout 600 'swaylock -f' before-sleep 'swaylock -f' &
