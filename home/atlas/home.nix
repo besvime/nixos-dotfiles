e20413e3c516bc97983c85d72a5863229c973f72
Cat <bodyglue4@gmail.com>
2025-11-05 00:26:01 -0500

    .

--- home/atlas/home.nix
+++ home/atlas/home.nix
@@ -34,7 +34,6 @@
   # configs
   home.file.".config/mango".source = ./config/mango;
   home.file.".config/waybar".source = ./config/waybar;
-  home.file.".config/rofi".source = ./config/rofi;
   home.file.".config/mako".source = ./config/mako;
   home.file.".config/fuzzel".source = ./config/fuzzel;
 
